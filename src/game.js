47003daf09fc5a741dc2e94025893bccd3a006c5
alexandreb42 <alexandre.bloch@epita.fr>
2020-01-17 11:14:00 +0100

    fonction scores un peu avancée

--- src/game.js
+++ src/game.js
@@ -1,6 +1,6 @@
 
 class Game {
-
+ 
     constructor() {
         this.results = [];
     }
@@ -15,12 +15,34 @@ class Game {
         else {
             
             this.results.push(nb);
-            return("On a fait tombé " + nb + " quilles !");
+            return("On a fait tomber " + nb + " quilles !");
         }
     }
 
     scores() {
-
+        let res = this.results;
+        let somme = res[0];
+        for (let tour=1; tour<res.length; tour++) {
+            
+            if (res[i] == 10) { // si strike
+                if(res[i+1] == 10) { // suivi d'un autre strike
+                    somme += 10;
+                    res[i] += somme;
+                }
+                else { // pas suivi de strike
+                    somme += res[i+1] + res[i+2];
+                    res[i] += somme;
+                }
+            }
+            else {
+                if (res[i] + res[i+1] == 10) { // si spare
+                    if(res[i+2] == 10) { // suivi d'un strike
+                        res[i] += 10;
+                    }
+                    res[i] += res[i+2];
+                }
+            }
+        }
     }
 }
 
--- test/game-test.js
+++ test/game-test.js
@@ -14,7 +14,7 @@ describe('Game', function() {
 
     it('should print the number of pins knocked down in 1 roll', function() {
         let game = new Game();
-        assert.equal( ("On a fait tombé 3 quilles !"), game.roll(3));
+        assert.equal( ("On a fait tomber 3 quilles !"), game.roll(3));
     })
 
     it('should have a results array', function() {
@@ -31,13 +31,13 @@ describe('Game', function() {
     it('numbers of pins should be more than 0', function() {
         let game = new Game();
         assert.equal(game.roll(-1), "Vous ne jouez pas le jeu !");
-        assert.equal(game.roll(0), "On a fait tombé 0 quilles !");
+        assert.equal(game.roll(0), "On a fait tomber 0 quilles !");
     })
 
     it('numbers of pins should be less than 10', function() {
         let game = new Game();
         assert.equal(game.roll(11), "Vous ne jouez pas le jeu !");
-        assert.equal(game.roll(10), "On a fait tombé 10 quilles !");
+        assert.equal(game.roll(10), "On a fait tomber 10 quilles !");
     })
 
     it('should have maximum 10 frames, so 20 rolls', function() {
@@ -48,6 +48,21 @@ describe('Game', function() {
         assert.equal(game.results.length, 20);
         assert.equal(game.roll(8), "Vous avez tous les scores d'une partie.");
         assert.equal(game.results.length, 20);
+    })
+
+    it('scores should be more than 0', function() {
+        let game = new Game();
+        assert.isAtLeast(game.scores(), 0);
+    })
+
+    it('scores should be less than 200', function() {
+        let game = new Game();
+        assert.isAtMost(game.scores(), 200);
+    })
+
+    it('scores should ', function() {
+        let game = new Game();
+        assert.isAtMost(game.scores(), 200);
     })
 
 })
